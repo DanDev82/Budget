bc2cf9aba1fd8694f7c5375f88542e19a44043db
Dan Coria <ifdanthencool@gmail.com>
2017-12-29 16:01:23 -0600

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+	$('#my_table').DataTable();
+
+
+
+
+	// $( function() {
+	//   $( ".datepicker" ).datepicker({
+	//   	autoclose: true
+	//   });
+	// });
+});
+
+
+
+
+// var config = {
+//   apiKey: "AIzaSyCw4fwCnaEDMm5QtqhfWk5FwPgdxD9gT5Q",
+//   authDomain: "my-budget-5c7f5.firebaseapp.com",
+//   databaseURL: "https://my-budget-5c7f5.firebaseio.com",
+//   projectId: "my-budget-5c7f5",
+//   storageBucket: "",
+//   messagingSenderId: "1013162195741"
+// };
+// firebase.initializeApp(config);
+
+// var messagesRef = firebase.database().ref('messages');
+
+
+
+
+
+
+
+// var dueDate = getInputVal('Due_Date').value;
+// var bills = getInputVal('Bills').value;
+// var amountDue = getInputVal('Due').value;
+// var budgeted = getInputVal('Budgeted').value;
+// var spentPaid = getInputVal('Spent_Paid').value;
+// var whatsLeft = getInputVal('Left').value;
+// var paychecks = getInputVal('Paychecks').value;
+// var allMonth = getInputVal('All_Month').value;
+// var leftOvers = getInputVal('Left_Overs').value;
+
+
+// saveMessage(dueDate, bills, amountDue, budgeted, spentPaid, whatsLeft, paychecks, allMonth, leftOvers);
+
+// function getInputVal(id){
+// 	return document.getElementById(id);
+// }
+
+// function saveMessage(dueDate, bills, amountDue, budgeted, spentPaid, whatsLeft, paychecks, allMonth, leftOvers){
+//   var newMessageRef = messagesRef.push();	
+//   newMessageRef.set({
+//     dueDate: dueDate,
+//     bills: bills,
+//     amountDue: amountDue,
+//     budgeted: budgeted,
+//     spentPaid: spentPaid,
+//     whatsLeft: whatsLeft,
+//     paychecks: paychecks,
+//     allMonth: allMonth,
+//     leftOvers: leftOvers
+//   });
+// }
+
+
